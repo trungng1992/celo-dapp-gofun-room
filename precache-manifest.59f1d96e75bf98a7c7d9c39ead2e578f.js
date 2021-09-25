f2b37db138032b387af81c8019d217e12007f641
trungng1992 <trung.ng1992@gmail.com>
2021-09-25 18:04:53 +0700

    Updates

---
+++ precache-manifest.59f1d96e75bf98a7c7d9c39ead2e578f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13e7f0571d09be8f5c14675c711aa39a",
+    "url": "/celo-dapp-gofun-room/index.html"
+  },
+  {
+    "revision": "85431b226285985afdc9",
+    "url": "/celo-dapp-gofun-room/static/css/2.7f2699be.chunk.css"
+  },
+  {
+    "revision": "6dbd9d2911f2f7881894",
+    "url": "/celo-dapp-gofun-room/static/css/main.d17a2d23.chunk.css"
+  },
+  {
+    "revision": "85431b226285985afdc9",
+    "url": "/celo-dapp-gofun-room/static/js/2.7d5f89ea.chunk.js"
+  },
+  {
+    "revision": "6dbd9d2911f2f7881894",
+    "url": "/celo-dapp-gofun-room/static/js/main.10b775bf.chunk.js"
+  },
+  {
+    "revision": "36b4be68ef33c823cafe",
+    "url": "/celo-dapp-gofun-room/static/js/runtime~main.c38dc722.js"
+  },
+  {
+    "revision": "6113d50582717422594e489365993fbd",
+    "url": "/celo-dapp-gofun-room/static/media/defaultBcg.6113d505.jpeg"
+  },
+  {
+    "revision": "33ab9f1e88f204b5b8a13725a918f36c",
+    "url": "/celo-dapp-gofun-room/static/media/details-1.33ab9f1e.jpeg"
+  },
+  {
+    "revision": "7345b9f61680c799868f66f297cedab5",
+    "url": "/celo-dapp-gofun-room/static/media/details-3.7345b9f6.jpeg"
+  },
+  {
+    "revision": "7569a31606be3db6b229327eec5999df",
+    "url": "/celo-dapp-gofun-room/static/media/details-4.7569a316.jpeg"
+  },
+  {
+    "revision": "cb0d9dacb84d9d1e923f3d50dd4c29d8",
+    "url": "/celo-dapp-gofun-room/static/media/nights2.cb0d9dac.jpeg"
+  },
+  {
+    "revision": "20021e3a70c0afd83b710eba1317aa6f",
+    "url": "/celo-dapp-gofun-room/static/media/ocean-room1.20021e3a.jpeg"
+  },
+  {
+    "revision": "fc3e910e81f562ba7868f3e8817806af",
+    "url": "/celo-dapp-gofun-room/static/media/room-2.fc3e910e.jpeg"
+  },
+  {
+    "revision": "f30e53c87a66e4183675c4548c7e4a8c",
+    "url": "/celo-dapp-gofun-room/static/media/view.f30e53c8.jpeg"
+  }
+]);
