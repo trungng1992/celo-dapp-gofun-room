0c4deb63d84e5df75c0bd37f23642bb849f54f74
trungng1992 <trung.ng1992@gmail.com>
2021-09-25 20:03:03 +0700

    commit

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Room from './Room'
 
 export default function RoomsList({ rooms }) {
-    if (rooms.length === 0) {
+    if (rooms.length == 0) {
         return (
             <div className="empty-search">
                 <h3>Unfortunately no rooms matched your search</h3>
--- src/pages/Add.js
+++ src/pages/Add.js
@@ -1,5 +1,4 @@
-import React, {useState, useEffect} from 'react';
-import { Link } from "react-router-dom";
+import React, {useState} from 'react';
 import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
